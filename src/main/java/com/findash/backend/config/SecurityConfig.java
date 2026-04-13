c9a3d182921a56e083589540a7b415b3e9cd611d
wolfnhk20 <ayushkulal20@gmail.com>
2026-04-14 00:39:48 +0530

    added HealthController

--- src/main/java/com/findash/backend/config/SecurityConfig.java
+++ src/main/java/com/findash/backend/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
 

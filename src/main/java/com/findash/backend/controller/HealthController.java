f8bc30077b06033733fcfa6f4c854a1d624da834
wolfnhk20 <ayushkulal20@gmail.com>
2026-04-14 00:38:55 +0530

    added HealthController

---
+++ src/main/java/com/findash/backend/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.findash.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
